8801abd2ef5218600516d01a4943e9a2df6ec339
m-h-miller <m.harry.miller@gmail.com>
2015-10-27 17:20:51 -0400

    Initialize repository; complete phases 1-4

---
+++ lib/phase5/params.rb
@@ -0,0 +1,53 @@
+require 'uri'
+
+module Phase5
+  class Params
+    # use your initialize to merge params from
+    # 1. query string
+    # 2. post body
+    # 3. route params
+    #
+    # You haven't done routing yet; but assume route params will be
+    # passed in as a hash to `Params.new` as below:
+    def initialize(req, route_params = {})
+      @params = {}
+
+      @params.merge!(route_params)
+      @params.merge!(parse_www_encoded_form(req.body)) if req.body
+      @params.merge!(parse_www_encoded_form(req.query_string)) if req.query_string
+
+    end
+
+    def [](key)
+      @params[key.to_s] || @params[key.to_sym]
+    end
+
+    # this will be useful if we want to `puts params` in the server log
+    def to_s
+      @params.to_s
+    end
+
+    class AttributeNotFoundError < ArgumentError; end;
+
+    private
+    # this should return deeply nested hash
+    # argument format
+    # user[address][street]=main&user[address][zip]=89436
+    # should return
+    # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
+    def parse_www_encoded_form(www_encoded_form)
+      params = {}
+
+      pairs_array = URI::decode_www_form(www_encoded_form)
+      pairs_array.each do |key, val|
+        params[key] = val
+      end
+      params
+    end
+
+    # this should return an array
+    # user[address][street] should return ['user', 'address', 'street']
+    def parse_key(key)
+    end
+  end
+end
